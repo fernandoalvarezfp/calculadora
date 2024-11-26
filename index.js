7402c5fa8eb92aa085daed2c2e9aca787af650fa
Fernando Álvarez Crespo <fernando@MacBook-Pro-de-Fernando.local>
2024-11-26 15:21:33 +0100

    Primer Commit

---
+++ index.js
@@ -0,0 +1,2 @@
+import { suma,resta,divide,multiplica } from "./src/calculadora.js"
+console.log(suma(2,3));
---
+++ src/calculadora.js
@@ -0,0 +1,12 @@
+const suma = (a, b) => a + b;
+const resta = (a, b) => a - b;
+const multiplica = (a, b) => a * b;
+const divide = (a, b) => {
+  if (b === 0) {
+    throw new Error("No podemos dividir entre cero");
+  } else {
+    return a / b;
+  }
+};
+
+module.exports = { suma, resta, multiplica, divide };
